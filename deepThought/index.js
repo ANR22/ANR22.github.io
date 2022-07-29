869d4f25f3f8d91855ae8f6bc60ebb870725d167
AMOGH N RAO <79016325+ANR22@users.noreply.github.com>
2022-07-29 19:47:24 +0530

    Add files via upload

---
+++ deepThought/index.js
@@ -0,0 +1,281 @@
+// import project from "./project.js";
+const project = {
+  _key: "topic:1053",
+  cid: null,
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p",
+  globalTags: [],
+  lastposttime: 0,
+  learning_outcomes: [
+    "learning outcome 1",
+    "learning outcome 2",
+    "learning outcome 3",
+  ],
+  mainPid: 0,
+  postcount: 0,
+  pre_requisites: ["pre-requisites 1", "pre-requisites 2", "pre-requisites 3"],
+  project_image: "https://media.giphy.com/media/l3vR85PnGsBwu1PFK/giphy.gif",
+  short_description: "",
+  slug: "1053/new-project",
+  start_time: 1653517339588,
+  status: "published",
+  tasks: [
+    {
+      task_id: 6233,
+      task_title: "demo task",
+      task_description: "desc desc",
+      tools: [],
+      assets: [
+        {
+          asset_id: 6234,
+          asset_title: "task heading 1",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type a",
+          asset_type: "display_asset",
+          asset_content: "reflection",
+          display_asset_url: "",
+          display_asset_image: "",
+          display_asset_video: "",
+          display_asset_docs: "",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop ",
+        },
+        {
+          asset_id: 6235,
+          asset_title: "task heading 2",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "input_asset",
+          asset_content: "tb",
+          display_asset_url: null,
+          display_asset_image: null,
+          display_asset_video: null,
+          display_asset_docs: null,
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6236,
+          asset_title: "task heading 3",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "input_asset",
+          asset_content: "reflection",
+          display_asset_url: null,
+          display_asset_image: null,
+          display_asset_video: null,
+          display_asset_docs: null,
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6237,
+          asset_title: "task heading 4",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "display_asset",
+          asset_content: "other",
+          display_asset_url: "",
+          display_asset_image: "",
+          display_asset_video: "",
+          display_asset_docs:
+            "https://drive.google.com/file/d/1Pxw83bTKE3KkOyYnwm1u6DSVjntSrNTp/preview?usp=sharing",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6238,
+          asset_title: "task heading 5",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "display_asset",
+          asset_content: "other",
+          display_asset_url: "",
+          display_asset_image:
+            "https://media.giphy.com/media/IzjhI7ggjDlEnMxZMu/giphy.gif",
+          display_asset_video: "",
+          display_asset_docs: "",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6239,
+          asset_title: "task heading 6",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "input_asset",
+          asset_content: "article",
+          display_asset_url: null,
+          display_asset_image: null,
+          display_asset_video: null,
+          display_asset_docs: null,
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6240,
+          asset_title: "task heading 7",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "input_asset",
+          asset_content: "eb",
+          display_asset_url: null,
+          display_asset_image: null,
+          display_asset_video: null,
+          display_asset_docs: null,
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6241,
+          asset_title: "task heading 8",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "display_asset",
+          asset_content: "other",
+          display_asset_url:
+            "https://drive.google.com/file/d/1dBBYQsz2yt3vc4YMkRmVC3nuTBbq2GON/preview?usp=sharing",
+          display_asset_image: "",
+          display_asset_video: "",
+          display_asset_docs: "",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+        {
+          asset_id: 6242,
+          asset_title: "task heading 9",
+          asset_description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+          asset_type: "display_asset",
+          asset_content: "other",
+          display_asset_url: "",
+          display_asset_image: "",
+          display_asset_video: "https://www.youtube.com/embed/s8H0Sx7jy7c",
+          display_asset_docs: "",
+          display_tb_tid: null,
+          display_tb_pid: null,
+          display_eb_tid: null,
+          display_eb_pid: null,
+          display_asset_reflection: null,
+        },
+      ],
+    },
+  ],
+  tid: 1053,
+  timestamp: 1653516431690,
+  title: "new Project",
+  tools: [],
+  type: "project",
+  uid: 100,
+  viewcount: 0,
+};
+
+const assetBody = (asset) => {
+  const { asset_type } = asset;
+
+  if (asset.asset_content == "tb") {
+    return '<img src="tb.png" class="tb" alt="tb" />';
+  } else if (asset.asset_content == "eb") {
+    return '<img src="eb.png" class="eb" alt="eb" />';
+  } else if (asset.display_asset_video) {
+    return `<iframe class='video' src=${asset.display_asset_video}></iframe>`;
+  } else if (asset.display_asset_docs) {
+    return `<div class='docs'><iframe src=${asset.display_asset_docs}></iframe></div>`;
+  } else if (asset.display_asset_image) {
+    return `<img class='image' src=${asset.display_asset_image} />`;
+  } else if (asset.display_asset_url) {
+    return `<iframe class='audio' src=${asset.display_asset_url}></iframe>`;
+  } else if (
+    asset.asset_content == "reflection" &&
+    asset_type == "input_asset"
+  ) {
+    return `<div class='article'><textarea rows='10'} placeholder='Reflect as per guidelines'></textarea></div>`;
+  } else if (asset.asset_content == "article") {
+    return `<div class='article'><textarea rows='10' placeholder='article'></textarea><button>Submit</button></div>`;
+  } else if (asset.display_asset_reflection) {
+    return `<p class='display-refelction'>${asset.display_asset_reflection}</p>`;
+  }
+};
+
+$(document).ready(() => {
+  const { tasks } = project;
+  $.each(tasks, (index, task) => {
+    $(`<span>${task.task_title}</span>`).appendTo(
+      ".task-container .task-title"
+    );
+    console.log(task.task_title);
+    var { assets } = task;
+    $(
+      `<a>${index + 1}. ${task.task_title}</a><ul class='${
+        task.task_title
+      }'></ul>`
+    ).appendTo(".assets-nav .asset-nav-body");
+    $.each(assets, (index, asset) => {
+      $(
+        `<div class='asset-container'>
+          <h4 class='asset-heading'>${asset.asset_title}</h4>
+           <div class="asset-body-container">
+           
+           ${assetBody(asset)}
+            
+           <p class='asset-description'>${asset.asset_description}</p>
+           <i id=${asset.asset_title} class="fa-solid fa-chevron-down"></i>
+           
+            </div>
+          </div>`
+      ).appendTo(".assets-container");
+
+      $(`<li>${asset.asset_title}</li>`).appendTo(`.assets-nav ul`);
+    });
+
+    var degrees = 0;
+    $(document).on("click", ".fa-chevron-down", (e) => {
+      const $clicked = e.target;
+      degrees += 180;
+      $($clicked).css("transform", "rotate(" + degrees + "deg)");
+      $($clicked).prev().toggle("slide");
+    });
+    var left = "0";
+
+    $(`<span class='no-of-tasks'>${tasks.length}</span>`).appendTo(
+      ".assets-nav"
+    );
+    $(document).on("click", ".fa-bars", () => {
+      $(".assets-nav").css("left", left + "px");
+      if (left == 0) {
+        $(".no-of-tasks").css("display", "none");
+        $(".asset-nav-body").css("display", "block");
+        left = -180;
+      } else {
+        $(".no-of-tasks").css("display", "block");
+        $(".asset-nav-body").css("display", "none");
+        left = 0;
+      }
+    });
+  });
+});
